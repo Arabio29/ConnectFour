837136fadefac1adb0cd21fd3ff5e055c1ca426b
Eliasib <ecantor.2906@gmail.com>
2024-08-07 00:26:51 -0500

    Agregar archivos de proyecto.

---
+++ ConnectFour/wwwroot/js/Alert.js
@@ -0,0 +1,36 @@
+﻿
+function winnerAlert(message){ 
+    Swal.fire({
+        title: message + " :)",
+        width: 600,
+        padding: "3em",
+        color: "#716add",
+        background: "#fff url(/images/trees.png)",
+        backdrop: `
+        rgba(0,0,123,0.4)
+        url("/images/nyan-cat.gif")
+        left top
+        no-repeat
+      `
+    });
+}
+
+/*alerta cambios de turno*/
+function turnAlert(mesagge) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "info",
+        title: "Shift :)",
+        text: mesagge
+    });
+}
